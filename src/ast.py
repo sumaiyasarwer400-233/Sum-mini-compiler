773bb948f6f99aee88eeec5b5de1e1f55bf53d7a
sumaiyasarwer400-233 <sumaiyasarwer400@gmail.com>
2026-09-25 15:10:05 +0600

    Add MiniLang AST structure

--- src/ast.py
+++ src/ast.py
@@ -1,172 +1,221 @@
 class ASTNode:
-    def __init__(self, node_type, **attributes):
-        self.node_type = node_type
-        self.attributes = attributes
+    """Base class for all AST nodes."""
 
-    def __repr__(self):
-        return self._format()
-
-    def _format(self, level=0):
-        space = "  " * level
-        result = f"{space}{self.node_type}"
-
-        for key, value in self.attributes.items():
-            result += f"\n{space}  {key}:"
-
-            if isinstance(value, ASTNode):
-                result += "\n" + value._format(level + 2)
-
-            elif isinstance(value, list):
-                for item in value:
-                    if isinstance(item, ASTNode):
-                        result += "\n" + item._format(level + 2)
-                    else:
-                        result += f"\n{space}    {item}"
-
-            else:
-                result += f" {value}"
-
-        return result
+    def __init__(self, line=0, column=0):
+        self.line = line
+        self.column = column
 
 
 class Program(ASTNode):
-    def __init__(self, statements):
-        super().__init__(
-            "Program",
-            statements=statements
-        )
-
-
-class Function(ASTNode):
-    def __init__(self, name, parameters, body):
-        super().__init__(
-            "Function",
-            name=name,
-            parameters=parameters,
-            body=body
-        )
+    def __init__(self, declarations, line=0, column=0):
+        super().__init__(line, column)
+        self.declarations = declarations
 
 
 class Block(ASTNode):
-    def __init__(self, statements):
-        super().__init__(
-            "Block",
-            statements=statements
-        )
+    def __init__(self, statements, line=0, column=0):
+        super().__init__(line, column)
+        self.statements = statements
 
 
-class Number(ASTNode):
-    def __init__(self, value):
-        super().__init__(
-            "Number",
-            value=value
-        )
+class VariableDeclaration(ASTNode):
+    def __init__(self, var_type, name, line=0, column=0):
+        super().__init__(line, column)
+        self.var_type = var_type
+        self.name = name
 
 
-class Boolean(ASTNode):
-    def __init__(self, value):
-        super().__init__(
-            "Boolean",
-            value=value
-        )
+class ArrayDeclaration(ASTNode):
+    def __init__(self, element_type, name, size, line=0, column=0):
+        super().__init__(line, column)
+        self.element_type = element_type
+        self.name = name
+        self.size = size
 
 
-class Identifier(ASTNode):
-    def __init__(self, name):
-        super().__init__(
-            "Identifier",
-            name=name
-        )
+class FunctionDeclaration(ASTNode):
+    def __init__(
+        self,
+        name,
+        parameters,
+        body,
+        return_type="int",
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.name = name
+        self.parameters = parameters
+        self.body = body
+        self.return_type = return_type
 
 
-class BinaryExpression(ASTNode):
-    def __init__(self, operator, left, right):
-        super().__init__(
-            "BinaryExpression",
-            operator=operator,
-            left=left,
-            right=right
-        )
-
-
-class UnaryExpression(ASTNode):
-    def __init__(self, operator, operand):
-        super().__init__(
-            "UnaryExpression",
-            operator=operator,
-            operand=operand
-        )
-
-
-class Assignment(ASTNode):
-    def __init__(self, left, right):
-        super().__init__(
-            "Assignment",
-            left=left,
-            right=right
-        )
+class Parameter(ASTNode):
+    def __init__(self, param_type, name, line=0, column=0):
+        super().__init__(line, column)
+        self.param_type = param_type
+        self.name = name
 
 
 class IfStatement(ASTNode):
-    def __init__(self, condition, then_branch, else_branch=None):
-        super().__init__(
-            "IfStatement",
-            condition=condition,
-            then_branch=then_branch,
-            else_branch=else_branch
-        )
+    def __init__(
+        self,
+        condition,
+        then_branch,
+        else_branch=None,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.condition = condition
+        self.then_branch = then_branch
+        self.else_branch = else_branch
 
 
 class WhileStatement(ASTNode):
-    def __init__(self, condition, body):
-        super().__init__(
-            "WhileStatement",
-            condition=condition,
-            body=body
-        )
+    def __init__(
+        self,
+        condition,
+        body,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.condition = condition
+        self.body = body
 
 
-class ForStatement(ASTNode):
-    def __init__(self, initialization, condition, update, body):
-        super().__init__(
-            "ForStatement",
-            initialization=initialization,
-            condition=condition,
-            update=update,
-            body=body
-        )
+class SwitchStatement(ASTNode):
+    def __init__(
+        self,
+        expression,
+        cases,
+        default_case=None,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.expression = expression
+        self.cases = cases
+        self.default_case = default_case
 
 
-class ReturnStatement(ASTNode):
-    def __init__(self, value):
-        super().__init__(
-            "ReturnStatement",
-            value=value
-        )
+class CaseStatement(ASTNode):
+    def __init__(
+        self,
+        value,
+        statements,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.value = value
+        self.statements = statements
 
 
 class BreakStatement(ASTNode):
-    def __init__(self):
-        super().__init__("BreakStatement")
+    def __init__(self, line=0, column=0):
+        super().__init__(line, column)
 
 
-class ContinueStatement(ASTNode):
-    def __init__(self):
-        super().__init__("ContinueStatement")
+class ReturnStatement(ASTNode):
+    def __init__(self, expression=None, line=0, column=0):
+        super().__init__(line, column)
+        self.expression = expression
 
 
-class CallExpression(ASTNode):
-    def __init__(self, name, arguments):
-        super().__init__(
-            "CallExpression",
-            name=name,
-            arguments=arguments
-        )
+class Assignment(ASTNode):
+    def __init__(
+        self,
+        target,
+        expression,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.target = target
+        self.expression = expression
+
+
+class ArrayAccess(ASTNode):
+    def __init__(
+        self,
+        name,
+        index,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.name = name
+        self.index = index
 
 
 class ExpressionStatement(ASTNode):
-    def __init__(self, expression):
-        super().__init__(
-            "ExpressionStatement",
-            expression=expression
-        )
+    def __init__(self, expression, line=0, column=0):
+        super().__init__(line, column)
+        self.expression = expression
+
+
+class BinaryExpression(ASTNode):
+    def __init__(
+        self,
+        left,
+        operator,
+        right,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+
+class UnaryExpression(ASTNode):
+    def __init__(
+        self,
+        operator,
+        operand,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.operator = operator
+        self.operand = operand
+
+
+class NumberLiteral(ASTNode):
+    def __init__(self, value, line=0, column=0):
+        super().__init__(line, column)
+        self.value = value
+
+
+class StringLiteral(ASTNode):
+    def __init__(self, value, line=0, column=0):
+        super().__init__(line, column)
+        self.value = value
+
+
+class BooleanLiteral(ASTNode):
+    def __init__(self, value, line=0, column=0):
+        super().__init__(line, column)
+        self.value = value
+
+
+class Identifier(ASTNode):
+    def __init__(self, name, line=0, column=0):
+        super().__init__(line, column)
+        self.name = name
+
+
+class FunctionCall(ASTNode):
+    def __init__(
+        self,
+        name,
+        arguments,
+        line=0,
+        column=0
+    ):
+        super().__init__(line, column)
+        self.name = name
+        self.arguments = arguments
